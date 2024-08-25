23c675916bbd70222dc2052e3dc8c352f57d5a6d
Edgardo <ecofre@gmail.com>
2024-08-25 17:02:46 -0400

    Added configuration to reads environment variables that allow to connect to an Azure SQL Server

--- src/main/java/com/example/demo/SpringBootWithAzureSqlApplication.java
+++ src/main/java/com/example/demo/SpringBootWithAzureSqlApplication.java
@@ -1,13 +1,17 @@
+
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootWithAzureSqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithAzureSqlApplication.class, args);
+		log.debug("[main][AZURE_SQL_SERVER_TEST_URL: {}]", System.getenv("AZURE_SQL_SERVER_TEST_URL"));
 	}
 
 }
